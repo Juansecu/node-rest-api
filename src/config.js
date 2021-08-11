abded17b5742a5e47d8398fe50a2dcd957d2f7cf
Juansecu <juansecoder@hotmail.com>
2021-08-11 15:48:23 -0500

    HashingSecret config was added to environments

--- src/config.js
+++ src/config.js
@@ -6,18 +6,21 @@ const environments = {};
 
 environments.development = {
     environment: 'development',
+    hashingSecret: 'sggs-55svssvg',
     httpPort: 3000,
     httpsPort: 3001
 };
 
 environments.production = {
     environment: 'production',
+    hashingSecret: 'stbvsrbbxxf$3wsh',
     httpPort: 4000,
     httpsPort: 443
 };
 
 environments.staging = {
     environment: 'staging',
+    hashingSecret: 'ksbsstgvced53bsvs%',
     httpPort: 5000,
     httpsPort: 5001
 };
