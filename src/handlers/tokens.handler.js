307054c0e6af6e55dbd37232a6a73f1a07b1ccbd
Juansecu <juansecoder@hotmail.com>
2021-08-12 13:10:40 -0500

    Functionality for post method from tokens handler was setted

--- src/handlers/tokens.handler.js
+++ src/handlers/tokens.handler.js
@@ -1,3 +1,7 @@
+const _dataLib = require('../lib/data.lib');
+
+const helpers = require('../helpers');
+
 // Container for the tokens submethods
 const _tokens = {};
 
@@ -14,7 +18,60 @@ _tokens.delete = (data, callback) => {};
 
 _tokens.get = (data, callback) => {};
 
-_tokens.post = (data, callback) => {};
+/**
+ * @param {{payload: {
+ *          password: string,
+ *          phoneNumber: string
+ * }}} data
+ */
+_tokens.post = (data, callback) => {
+    const password =
+        typeof data.payload.password === 'string' &&
+        data.payload.password.match(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,20}$/
+        )
+            ? data.payload.password.trim()
+            : '';
+    const phoneNumber =
+        typeof data.payload.phoneNumber === 'string' &&
+        data.payload.phoneNumber.match(/^\+?\d{9,15}$/)
+            ? data.payload.phoneNumber.trim()
+            : '';
+
+    if (password && phoneNumber) {
+        // Lookup the user
+        _dataLib.read(
+            phoneNumber.replace('+', ''),
+            'users',
+            (error, userData) => {
+                if (!error) {
+                    // Hash the sent password, and compare it to the stored password
+                    const passwordHash = helpers.hash(password);
+
+                    if (passwordHash === userData.password) {
+                        // If valid, create a new token with a random name. Set expiration to 1 hour from now
+                        const tokenId = helpers.createRandomString(20);
+                        const tokenExpiration = Date.now() + 3600000;
+                        const tokenData = {
+                            tokenId,
+                            phoneNumber,
+                            tokenExpiration
+                        };
+
+                        // Save the token
+                        _dataLib.write(tokenData, tokenId, 'tokens', error => {
+                            if (!error) callback(null, { token: tokenId });
+                            else
+                                callback(500, {
+                                    message: 'Error creating token'
+                                });
+                        });
+                    } else callback(401, { message: 'Incorrect password' });
+                } else callback(404, { message: 'User not found' });
+            }
+        );
+    } else callback(400, { message: 'Missing required fields' });
+};
 
 _tokens.put = (data, callback) => {};
 
