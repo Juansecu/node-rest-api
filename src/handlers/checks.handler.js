7c1837f4ee2135fb0a9008ce87c0217b8005452d
Juansecu <juansecoder@hotmail.com>
2021-08-13 14:43:53 -0500

    Functionality for put method from checksHandler was setted

--- src/handlers/checks.handler.js
+++ src/handlers/checks.handler.js
@@ -216,6 +216,129 @@ _checks.post = (data, callback) => {
     } else callback(400, { message: 'Missing required fields' });
 };
 
-_checks.put = (data, callback) => {};
+/**
+ * @param {{
+ *     headers: {
+ *         authorization: string
+ *     },
+ *     payload: {
+ *         checkId: string,
+ *         method?: string,
+ *         protocol?: string,
+ *         successCodes?: number[],
+ *         timeoutSeconds?: number,
+ *         url?: string,
+ *     }
+ * }} data
+ * @param {(
+ *           statusCode: number,
+ *          messageObject: { message: string }
+ * )} callback
+ */
+_checks.put = (data, callback) => {
+    // Check that all the required fields are present
+    const checkId =
+        typeof data.payload.checkId === 'string' &&
+        data.payload.checkId.match(/^[a-zA-Z0-9]{20}$/)
+            ? data.payload.checkId.trim()
+            : '';
+
+    // Check for the optional fields
+    const method =
+        typeof data.payload.method === 'string' &&
+        ['DELETE', 'GET', 'POST', 'PUT'].indexOf(
+            data.payload.method.toUpperCase()
+        ) > -1
+            ? data.payload.method
+            : '';
+    const protocol =
+        typeof data.payload.protocol === 'string' &&
+        ['http', 'https'].indexOf(data.payload.protocol) > -1
+            ? data.payload.protocol
+            : '';
+    const successCodes =
+        typeof data.payload.successCodes === 'object' &&
+        data.payload.successCodes instanceof Array &&
+        data.payload.successCodes.length &&
+        data.payload.successCodes.every(
+            code => typeof code === 'number' && code > 0
+        )
+            ? data.payload.successCodes
+            : [];
+    const timeoutSeconds =
+        typeof data.payload.timeoutSeconds === 'number' &&
+        data.payload.timeoutSeconds % 1 === 0 &&
+        data.payload.timeoutSeconds > 0 &&
+        data.payload.timeoutSeconds < 6
+            ? data.payload.timeoutSeconds
+            : 0;
+    const url =
+        typeof data.payload.url === 'string' && data.payload.url.length
+            ? data.payload.url
+            : '';
+
+    if (checkId) {
+        if (
+            method ||
+            protocol ||
+            successCodes.length ||
+            timeoutSeconds ||
+            url
+        ) {
+            // Lookup the check by reading the ID
+            _dataLib.read(checkId, 'checks', (error, checkData) => {
+                if (!error) {
+                    // Get the token from the headers
+                    const token =
+                        typeof data.headers.authorization === 'string' &&
+                        data.headers.authorization.match(/^[a-zA-Z0-9]{20}$/)
+                            ? data.headers.authorization.trim()
+                            : '';
+
+                    _tokens.verifyToken(
+                        token,
+                        checkData.userPhoneNumber,
+                        valid => {
+                            if (valid) {
+                                // Update the check where necessary
+                                if (method) checkData.method = method;
+                                if (protocol) checkData.protocol = protocol;
+                                if (successCodes)
+                                    checkData.successCodes = successCodes;
+                                if (timeoutSeconds)
+                                    checkData.timeoutSeconds = timeoutSeconds;
+                                if (url) checkData.url = url;
+
+                                // Save the check
+                                _dataLib.update(
+                                    checkData,
+                                    checkId,
+                                    'checks',
+                                    error => {
+                                        if (!error) {
+                                            // Return the check data
+                                            callback(200, {
+                                                check: checkData
+                                            });
+                                        } else
+                                            callback(500, {
+                                                message: 'Error updating check'
+                                            });
+                                    }
+                                );
+                            } else
+                                callback(401, {
+                                    message: 'Unauthorized'
+                                });
+                        }
+                    );
+                } else
+                    callback(404, {
+                        message: 'Check not found'
+                    });
+            });
+        } else callback(400, { message: 'Nothing to update' });
+    } else callback(400, { message: 'Missing check ID' });
+};
 
 module.exports = checksHandler;
