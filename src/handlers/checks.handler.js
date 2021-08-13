35498b8879b56829e5605e3149a25ad06ffcad1b
Juansecu <juansecoder@hotmail.com>
2021-08-13 10:40:14 -0500

    Functionality for post method from checksHandler was setted

--- src/handlers/checks.handler.js
+++ src/handlers/checks.handler.js
@@ -1,3 +1,9 @@
+const config = require('../config');
+
+const _dataLib = require('../lib/data.lib');
+
+const helpers = require('../helpers');
+
 // Container for the checks submethods
 const _checks = {};
 
@@ -14,7 +20,153 @@ _checks.delete = (data, callback) => {};
 
 _checks.get = (data, callback) => {};
 
-_checks.post = (data, callback) => {};
+/**
+ * @param {{
+ *     headers: {
+ *        authorization: string
+ *     }
+ *     payload: {
+ *         method: string,
+ *         protocol: string,
+ *         successCodes: number[],
+ *         timeoutSeconds: number,
+ *         url: string
+ *     }
+ * }} data
+ * @param {(
+ *          statusCode: number,
+ *          messageObject: { message: string }
+ * )} callback
+ */
+_checks.post = (data, callback) => {
+    // Check that all the required fields are present
+    const method =
+        typeof data.payload.method === 'string' &&
+        ['DELETE', 'GET', 'POST', 'PUT'].indexOf(
+            data.payload.method.toUpperCase()
+        ) > -1
+            ? data.payload.method
+            : '';
+    const protocol =
+        typeof data.payload.protocol === 'string' &&
+        ['http', 'https'].indexOf(data.payload.protocol) > -1
+            ? data.payload.protocol
+            : '';
+    const successCodes =
+        typeof data.payload.successCodes === 'object' &&
+        data.payload.successCodes instanceof Array &&
+        data.payload.successCodes.length &&
+        data.payload.successCodes.every(
+            code => typeof code === 'number' && code > 0
+        )
+            ? data.payload.successCodes
+            : [];
+    const timeoutSeconds =
+        typeof data.payload.timeoutSeconds === 'number' &&
+        data.payload.timeoutSeconds % 1 === 0 &&
+        data.payload.timeoutSeconds > 0 &&
+        data.payload.timeoutSeconds < 6
+            ? data.payload.timeoutSeconds
+            : 0;
+    const url =
+        typeof data.payload.url === 'string' && data.payload.url.length
+            ? data.payload.url
+            : '';
+
+    if (method && protocol && successCodes.length && timeoutSeconds && url) {
+        // Get the token from the headers
+        const token =
+            typeof data.headers.authorization === 'string' &&
+            data.headers.authorization.match(/^[a-zA-Z0-9]{20}$/)
+                ? data.headers.authorization.trim()
+                : '';
+
+        // Lookup the user by reading the token
+        _dataLib.read(token, 'tokens', (error, tokenData) => {
+            if (!error) {
+                const userPhoneNumber = tokenData.phoneNumber;
+
+                // Lookup the user by reading the phone number
+                _dataLib.read(
+                    userPhoneNumber.replace('+', ''),
+                    'users',
+                    (error, userData) => {
+                        if (!error) {
+                            const userChecks =
+                                typeof userData.checks === 'object' &&
+                                userData.checks instanceof Array
+                                    ? userData.checks
+                                    : [];
+
+                            // Verify that the user has less than the maximum number of checks
+                            if (userChecks.length < config.maxChecks) {
+                                // Create a random ID for the check
+                                const checkId = helpers.createRandomString(20);
+
+                                // Create the check object, and include the user's phone number
+                                const check = {
+                                    checkId,
+                                    userPhoneNumber,
+                                    protocol,
+                                    url,
+                                    method,
+                                    successCodes,
+                                    timeoutSeconds
+                                };
+
+                                // Save the check
+                                _dataLib.write(
+                                    check,
+                                    checkId,
+                                    'checks',
+                                    error => {
+                                        if (!error) {
+                                            // Add the check ID to the user's checks
+                                            userData.checks = userChecks;
+                                            userData.checks.push(checkId);
+
+                                            // Save the new user data
+                                            _dataLib.update(
+                                                userData,
+                                                userPhoneNumber.replace(
+                                                    /\+/,
+                                                    ''
+                                                ),
+                                                'users',
+                                                error => {
+                                                    if (!error) {
+                                                        // Return the check data
+                                                        callback(200, {
+                                                            check
+                                                        });
+                                                    } else
+                                                        callback(500, {
+                                                            message:
+                                                                'Error updating user data with the new check'
+                                                        });
+                                                }
+                                            );
+                                        } else
+                                            callback(500, {
+                                                message: 'Error saving check'
+                                            });
+                                    }
+                                );
+                            } else
+                                callback(429, {
+                                    message:
+                                        'You have reached the maximum number of checks per user'
+                                });
+                        } else
+                            callback(404, {
+                                message: 'User not found'
+                            });
+                    }
+                );
+            } else callback(401, { message: 'Unauthorized' });
+        });
+    } else callback(400, { message: 'Missing required fields' });
+};
 
 _checks.put = (data, callback) => {};
 
